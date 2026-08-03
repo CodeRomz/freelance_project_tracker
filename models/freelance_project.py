6cdf22d8732a54163108676e19c3a8f0c3603c08
CodeRomz <romualdojrb@yahoo.com>
2026-08-03 15:40:29 +0400

    .

--- models/freelance_project.py
+++ models/freelance_project.py
@@ -14,3 +14,19 @@ class FreelanceProject(models.Model):
     freelancer_id = fields.Many2one("res.partner", string='Freelancer')
     category_id = fields.Many2one('project.category', string='Category')
 
+    def claim_project(self):
+        for record in self:
+            if record.state == 'completed':
+                raise UserError(_('This project is already completed.'))
+            elif not record.freelancer_id:
+                raise UserError(_('Please assign this project to a freelancer first'))
+            else:
+                record.state = 'progress'
+
+    def complete_project(self):
+        for record in self:
+            if record.state == 'open' and not record.freelancer_id:
+                raise UserError(_('Please assign this project to a freelancer'))
+            else:
+                record.state = 'completed'
+
