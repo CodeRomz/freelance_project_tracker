6e483578b7fbafb194b4c5ab2839fd0d5dfc42f3
CodeRomz <romualdojrb@yahoo.com>
2026-08-03 14:35:40 +0400

    .

--- __manifest__.py
+++ __manifest__.py
@@ -8,6 +8,7 @@
     'data': [
             "views/freelance_project_views.xml",
             "views/freelance_marketplace_menus.xml",
+            "views/freelance_project_category_views.xml",
             "security/ir.model.access.csv"
     ],
 
