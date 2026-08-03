1e95d21a122d2f5a1d2b82faf06dbd0e74626658
CodeRomz <romualdojrb@yahoo.com>
2026-08-03 12:02:33 +0400

    .

--- __manifest__.py
+++ __manifest__.py
@@ -6,6 +6,9 @@
     'application' : True,
 
     'data': [
+            "views/freelance_project_views.xml",
+            "views/freelance_marketplace_menus.xml",
+            "security/ir.model.access.csv"
     ],
 
 
