9e654808fed3d322b8764a37af2d057127e9f224
CodeRomz <romualdojrb@yahoo.com>
2026-08-03 15:04:08 +0400

    .

--- __manifest__.py
+++ __manifest__.py
@@ -3,14 +3,13 @@
 
     'depends': ['base'],
 
-    'application' : True,
+    'application': True,
 
-    'data': [
-            "views/freelance_project_views.xml",
-            "views/freelance_marketplace_menus.xml",
-            "views/freelance_project_category_views.xml",
-            "security/ir.model.access.csv"
+    "data": [
+        "security/ir.model.access.csv",
+        "views/freelance_project_views.xml",
+        "views/project_category_views.xml",
+        "views/freelance_marketplace_menus.xml",
     ],
-
 
 }
